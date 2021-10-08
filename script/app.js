45eff112e45a86f4cc10f2f091e5b3edd43e1a4d
kamron <abduhafizovk@gmail.com>
2021-10-08 20:05:11 +0300

    kamron

---
+++ script/app.js
@@ -0,0 +1,14 @@
+  
+  do {
+    let = name = prompt("sayt kimniki?")
+  } while (name !== "Kamron")
+  
+  alert("tog'ri, endi parolni kiriting")
+  
+  
+do {
+    let = password = +prompt("parolni kiriting")
+} while (password !== 2000);
+    alert("Parol to'ri <<OK>> ni bosib saytga kirishingiz mumkin😎")
+    
+    
